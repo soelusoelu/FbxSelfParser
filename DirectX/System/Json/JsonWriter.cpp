3315974d9d6b1fbe76bcdcc25972e251b8c7c6fb
takano <llmn.0419@gmail.com>
2021-10-10 00:38:36 +0900

    Jsonファイルの出力修正

--- DirectX/System/Json/JsonWriter.cpp
+++ DirectX/System/Json/JsonWriter.cpp
@@ -1,29 +1,40 @@
 ﻿#include "JsonWriter.h"
 #include "JsonObject.h"
+#include "JsonOutputStream.h"
 #include "JsonValue.h"
+#include "../../Math/Math.h"
 #include <cassert>
+#include <fstream>
 
 JsonWriter::JsonWriter() = default;
 
 JsonWriter::~JsonWriter() = default;
 
 void JsonWriter::write(const char* filePath, const JsonObject& inObject) const {
+    JsonOutputStream stream;
+
+    writeObjectStart(stream);
+    writeNewLine(stream);
+
+    int tabCount = 0;
+    writeValues(stream, inObject, tabCount);
+
+    writeObjectEnd(stream);
+
+    //文字列の末尾にヌル文字を追加して文字列化は終わり
+    stream.addSize(1);
+    stream.push('\0');
+
     //ファイルに書き込む
     std::ofstream outFile(filePath);
     if (!outFile.is_open()) {
         return;
     }
 
-    outFile << '{';
-    writeNewLine(outFile);
-
-    int tabCount = 0;
-    writeValues(outFile, inObject, tabCount);
-
-    outFile << '}';
+    outFile << stream.getStringBufer();
 }
 
-void JsonWriter::writeValues(std::ofstream& out, const JsonObject& inObject, int& tabCount) const {
+void JsonWriter::writeValues(JsonOutputStream& out, const JsonObject& inObject, int& tabCount) const {
     ++tabCount;
 
     const auto& values = inObject.values;
@@ -45,7 +56,7 @@ void JsonWriter::writeValues(std::ofstream& out, const JsonObject& inObject, int
 
         --num;
         if (num > 0) {
-            out << ',';
+            writeComma(out);
         }
 
         writeNewLine(out);
@@ -54,13 +65,13 @@ void JsonWriter::writeValues(std::ofstream& out, const JsonObject& inObject, int
     --tabCount;
 }
 
-void JsonWriter::writeValue(std::ofstream& out, const JsonValue& inValue, int& tabCount) const {
+void JsonWriter::writeValue(JsonOutputStream& out, const JsonValue& inValue, int& tabCount) const {
     if (inValue.isBool()) {
-        out << (inValue.getBool() ? true : false);
+        writeBool(out, inValue.getBool());
     } else if (inValue.isInt()) {
-        out << inValue.getInt();
+        writeInt(out, inValue.getInt());
     } else if (inValue.isFloat()) {
-        out << std::to_string(inValue.getFloat());
+        writeFloat(out, inValue.getFloat());
     } else if (inValue.isString()) {
         writeString(out, inValue.getString());
     } else if (inValue.isArray()) {
@@ -72,11 +83,11 @@ void JsonWriter::writeValue(std::ofstream& out, const JsonValue& inValue, int& t
     }
 }
 
-void JsonWriter::writeObject(std::ofstream& out, const JsonObject& inObject, int& tabCount) const {
-    out << '{';
+void JsonWriter::writeObject(JsonOutputStream& out, const JsonObject& inObject, int& tabCount) const {
+    writeObjectStart(out);
     //空オブジェクトなら早期リターン
     if (inObject.values.empty()) {
-        out << '}';
+        writeObjectEnd(out);
         return;
     }
 
@@ -86,14 +97,14 @@ void JsonWriter::writeObject(std::ofstream& out, const JsonObject& inObject, int
     writeValues(out, inObject, tabCount);
 
     writeTab(out, tabCount);
-    out << '}';
+    writeObjectEnd(out);
 }
 
-void JsonWriter::writeArray(std::ofstream& out, const std::vector<JsonValue>& inValues, int& tabCount) const {
-    out << '[';
+void JsonWriter::writeArray(JsonOutputStream& out, const std::vector<JsonValue>& inValues, int& tabCount) const {
+    writeArrayStart(out);
     //空配列なら早期リターン
     if (inValues.empty()) {
-        out << ']';
+        writeArrayEnd(out);
         return;
     }
 
@@ -110,7 +121,7 @@ void JsonWriter::writeArray(std::ofstream& out, const std::vector<JsonValue>& in
 
         --num;
         if (num > 0) {
-            out << ',';
+            writeComma(out);
         }
 
         writeNewLine(out);
@@ -119,23 +130,131 @@ void JsonWriter::writeArray(std::ofstream& out, const std::vector<JsonValue>& in
     --tabCount;
 
     writeTab(out, tabCount);
-    out << ']';
+    writeArrayEnd(out);
 }
 
-void JsonWriter::writeTab(std::ofstream& out, int count) const {
-    for (int i = 0; i < count; ++i) {
-        out << '\t';
+void JsonWriter::writeInt(JsonOutputStream& out, int in) const {
+    auto s = std::to_string(in);
+    out.addSize(s.length());
+    for (const auto& c : s) {
+        out.push(c);
     }
 }
 
-void JsonWriter::writeNewLine(std::ofstream& out) const {
-    out << '\n';
+void JsonWriter::writeFloat(JsonOutputStream& out, float in) const {
+    //値が0なら0.0を入力して終わり
+    if (Math::nearZero(in)) {
+        out.addSize(3);
+        out.push('0');
+        out.push('.');
+        out.push('0');
+        return;
+    }
+
+    //数値を文字列化
+    auto s = std::to_string(in);
+
+    //文字列にした際の無駄な0を削除する
+    while (s.back() == '0') {
+        s.pop_back();
+    }
+    //小数部分がすべて0だったとき最後に0を足す
+    if (s.back() == '.') {
+        s += '0';
+    }
+
+    out.addSize(s.length());
+    for (const auto& c : s) {
+        out.push(c);
+    }
 }
 
-void JsonWriter::writeColon(std::ofstream& out) const {
-    out << ": ";
+void JsonWriter::writeBool(JsonOutputStream& out, bool in) const {
+    if (in) {
+        out.addSize(4);
+        out.push('t');
+        out.push('r');
+        out.push('u');
+        out.push('e');
+    } else {
+        out.addSize(5);
+        out.push('f');
+        out.push('a');
+        out.push('l');
+        out.push('s');
+        out.push('e');
+    }
 }
 
-void JsonWriter::writeString(std::ofstream& out, const std::string& in) const {
-    out << '"' + in + '"';
+void JsonWriter::writeString(JsonOutputStream& out, const std::string& in) const {
+    //文字列数 + ダブルクォーテーション2つ分
+    out.addSize(in.length() + 2);
+
+    out.push('"');
+
+    for (auto itr = in.begin(); itr < in.end(); ++itr) {
+        char c = *itr;
+
+        if (c == '\\') {
+            //ファイルパス文字列の正規表現操作
+            out.push(c);
+
+            ++itr;
+            char next = *itr;
+            if (next != '\\') {
+                out.addSize(1);
+                out.push('\\');
+            }
+
+            out.push(next);
+        } else {
+            out.push(c);
+        }
+    }
+
+    out.push('"');
+}
+
+void JsonWriter::writeObjectStart(JsonOutputStream& out) const {
+    out.addSize(1);
+    out.push('{');
+}
+
+void JsonWriter::writeObjectEnd(JsonOutputStream& out) const {
+    out.addSize(1);
+    out.push('}');
+}
+
+void JsonWriter::writeArrayStart(JsonOutputStream& out) const {
+    out.addSize(1);
+    out.push('[');
+}
+
+void JsonWriter::writeArrayEnd(JsonOutputStream& out) const {
+    out.addSize(1);
+    out.push(']');
+}
+
+void JsonWriter::writeComma(JsonOutputStream& out) const {
+    out.addSize(1);
+    out.push(',');
+}
+
+void JsonWriter::writeTab(JsonOutputStream& out, int count) const {
+    out.addSize(count);
+
+    for (int i = 0; i < count; ++i) {
+        out.push('\t');
+    }
+}
+
+void JsonWriter::writeNewLine(JsonOutputStream& out) const {
+    out.addSize(1);
+    out.push('\n');
+}
+
+void JsonWriter::writeColon(JsonOutputStream& out) const {
+    out.addSize(2);
+    out.push(':');
+    out.push(' ');
 }
